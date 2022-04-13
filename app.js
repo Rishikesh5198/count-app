f89e0bc9f092edf5aab2c9f052a846d434ace56e
Rishikesh chava <102192822+Rishikesh5198@users.noreply.github.com>
2022-04-12 18:55:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function increase()
+{
+
+    let a=document.getElementById("shivi");
+    a.textContent++;
+    
+}
+function decrease()
+{
+    let a=document.getElementById("shivi");
+    a.textContent--;
+}
+function reset()
+{
+    let a=document.getElementById("shivi")
+    a.textContent=0;
+}
